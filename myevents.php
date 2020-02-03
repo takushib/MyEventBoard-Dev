1f3f4c1987f2a76e85331523a3474acb0455e3eb
Simouie <simon-louie@outlook.com>
2020-02-03 01:39:36 -0800

    Implement ability to view all events

--- myevents.php
+++ myevents.php
@@ -5,6 +5,41 @@
     $loader = new Twig_Loader_Filesystem('my_events_templates');
     $twig = new Twig_Environment($loader);
 
-    echo $twig -> render('views/events_index.twig'); 
+    // set up connection to database via MySQLi
+
+    include 'php/database.php';
+
+    // get invitation data from database
+
+    $query = "
+    
+        SELECT 
+            Event.id AS 'Event ID',
+            Event.name AS 'Name',
+            User.onid AS 'Creator',
+            Event.capacity AS 'Capacity',
+            Event.open_slots AS 'Available Slots'
+        FROM 
+            Event, User
+        WHERE
+            Event.fk_event_creator = User.id
+        
+    ";
+
+    $statement = $database -> prepare($query);
+    $statement -> execute();
+
+    $result = $statement -> get_result();
+
+    $result_array = $result -> fetch_all(MYSQLI_ASSOC);
+
+    $result_keys = array_keys($result_array[0]);
+
+    // render page using twig
+
+    echo $twig -> render(
+        'views/events_index.twig', 
+        ['table_headers' => $result_keys, 'table_rows' => $result_array]
+    ); 
 
 ?>
