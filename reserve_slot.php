da87176fcfc73b9351f13cdab7c6eaa42e64fd78
Simouie <simon-louie@outlook.com>
2020-02-07 01:47:42 -0800

    Add more feedback for time slot sign-up

--- reserve_slot.php
+++ reserve_slot.php
@@ -1,8 +1,5 @@
 <?php
 
-
-  require 'vendor/autoload.php';
-
   include 'php/database.php';
 
   $slot_id = $_POST["key"];
@@ -18,11 +15,5 @@
   $result = $database->query($sql);
   $row = $result->fetch_array(MYSQLI_NUM);
   echo $row[0];
-
-
-
-
-
-
-
+  
 ?>
