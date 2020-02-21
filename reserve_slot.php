cf8a42988db896a285dfae8fc688a1196a0c9cea
Blaise Takushi <takushib@flip2.engr.oregonstate.edu>
2020-02-21 12:54:31 -0800

    configured email confirmation

--- reserve_slot.php
+++ reserve_slot.php
@@ -14,6 +14,21 @@
   $sql = "SELECT @res1";
   $result = $database->query($sql);
   $row = $result->fetch_array(MYSQLI_NUM);
+  if ($row[0] != -1) {
+    $queryUser = 'SELECT email, first_name FROM User WHERE id = ?';
+    $statement2 = $database->prepare($queryUser);
+    $statement2->bind_param("i", $user_id);
+    $statement2->execute();
+    $res = $statement2->get_result();
+    $user = $res->fetch_object();
+    $format = "Hi %s,\nYou have successfully reserved a timeslot.\n";
+    $headers = "From: MyEventBoard"
+    . "\r\n" .
+    "CC: louiesi@oregonstate.edu, liaoto@oregonstate.edu";
+    $msg = sprintf($format, $user->first_name);
+    mail($user->email,"Confirmation",$msg,$headers);
+  }
   echo $row[0];
-  
+
+
 ?>
