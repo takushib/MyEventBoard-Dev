8b92cb0b9619e6ee95a08c1bf653298f16a009b2
Blaise Takushi <takushib@flip3.engr.oregonstate.edu>
2020-02-24 00:22:42 -0800

    dashboard works with database, just need to change date format

--- view_js/main.js
+++ view_js/main.js
@@ -2,42 +2,42 @@ function createEventBlock(eventName, creatorName, slotsRemaining, eventTime)
 {
 	var titleContainer = $('<div></div>');
 	titleContainer.addClass("titleContainer");
-	
+
 	var newEvent = $('<div></div>');
-	
+
 	var newEventName = $('<div><text></text></div>');
 	var eventNameText = eventName;
-	
+
 	//var newEventIcon = $('<div><img src="./icon.png"></img></div>');
 	var newEventIcon = $('<img src="./icon.png"></img>');
-	
+
 	var newEventCreator = $('<div><text></text></div>');
 	var creatorText = creatorName;
-	
+
 	var timeOfEvent = $('<div><text></text></div>');
-	
+
 	var newEventAvailSlot = $('<div><text></text></div>');
 	var slotsText = "Slots: ";
 	slotsText = slotsText + slotsRemaining + " remaining";
-	
+
 	newEventName.text(eventNameText);
 	newEventName.addClass("eventBlockName");
 	newEventName.addClass("Container");
-	
+
 	newEventIcon.addClass("eventIcon");
-	
-	
+
+
 	newEventCreator.text(creatorText);
 	newEventCreator.addClass("eventBlockCreator");
 	newEventCreator.addClass("Container");
-	
+
 	timeOfEvent.text(eventTime);
 	timeOfEvent.addClass("eventBlockSpace");
 	timeOfEvent.addClass("Container");
-	
+
 	//newEventAvailSlot.text(slotsText);
 	//newEventAvailSlot.addClass("eventBlockSpace");
-	
+
 	titleContainer.append(newEventName);
 	titleContainer.append(newEventIcon);
 	//newEvent.append(newEventName);
@@ -46,35 +46,27 @@ function createEventBlock(eventName, creatorName, slotsRemaining, eventTime)
 	newEvent.append(newEventCreator);
 	newEvent.append(timeOfEvent);
 	//newEvent.append(newEventAvailSlot);
-	
-	
+
+
 	newEvent.addClass("eventBlock");
 	return newEvent;
 }
 
 $(document).ready(function () {
-	
-	createEventBlock("Test Event",        "MyEventBoard Co.",    "10", "5:00 PM");
-	
-	$('.eventsContainer').append(newEvent); 
-	
-	var newEvent;
-	
-	newEvent = createEventBlock("CS 392 Recitation", "CS 392 TA",           "1",  "6:30 PM");
-	$('.eventsContainer').append(newEvent); 
-	newEvent = createEventBlock("Uploaded Event",    "MyEventBoard Co.",    "3",  "2:20 PM");
-	$('.eventsContainer').append(newEvent); 
-	newEvent = createEventBlock("Party Night",       "Jake Farst",          "5",  "1:15 PM");
-	$('.eventsContainer').append(newEvent); 
-	newEvent = createEventBlock("Party Night that's fun for tonight but it's ",       "Jake Farst",  "5",  "1:15 PM");
-	$('.eventsContainer').append(newEvent); 
-	newEvent = createEventBlock("Party Night that's fun for tonight but it's ",       "Jake Farst",  "5",  "1:15 PM");
-	$('.eventsContainer').append(newEvent); 
-	newEvent = createEventBlock("Party Night that's fun for tonight but it's ",       "Jake Farst",  "5",  "1:15 PM");
-	$('.eventsContainer').append(newEvent); 
-	newEvent = createEventBlock("Party Night that's fun for tonight but it's ",       "Jake Farst",  "5",  "1:15 PM");
-	newEvent.addClass("finishedEvent");
-	$('.eventsContainer').append(newEvent); 
+	var events;
+	$.ajax({
+		url:"main_dashboard.php",
+		type: "POST",
+		data: {user_id: 'takushib'},
+	}).done(function(response) {
+		events = JSON.parse(response);
+		console.log(events.length);
+		for (let i = 0; i < events.length; i++) {
+			var newEvent;
+			newEvent = createEventBlock(events[i].event_name, events[i].ec_first_name, events[i].slots_remaining,  events[i].start_time);
+			$('.eventsContainer').append(newEvent);
+		}
+	});
 })
 
 $(document).ready(function () {
