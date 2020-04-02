5d4ea2b98d5fccecbd2b8bd79eb684106aa5be25
Simouie <simon-louie@outlook.com>
2020-04-02 09:22:23 -0700

    Adjust height and width for logo

--- view_js/main.js
+++ view_js/main.js
@@ -66,7 +66,7 @@ function createEventBlock(eventName, eventDate, creatorName, slotsRemaining, eve
 	var eventNameText = eventName;
 
 	//var newEventIcon = $('<div><img src="./icon.png"></img></div>');
-	var newEventIcon = $('<img src="MyEventBoardLogos/MEB_logo-04.png"></img>');
+	var newEventIcon = $('<div><img src="MyEventBoardLogos/MEB_logo-04.png"></img></div>');
 
 	var newEventCreator = $('<div><text></text></div>');
 	var creatorText = creatorName;
@@ -83,7 +83,6 @@ function createEventBlock(eventName, eventDate, creatorName, slotsRemaining, eve
 	newEventName.addClass("Container");
 
 	newEventIcon.addClass("eventIcon");
-
 
 	newEventCreator.text(creatorText);
 	newEventCreator.addClass("eventBlockCreator");
