c292dee0e6f718077f35a6b2075562b0a59fc594
Blaise Takushi <takushib@flip3.engr.oregonstate.edu>
2020-04-06 19:07:20 -0700

    finished email confirmation

--- edit_event.php
+++ edit_event.php
@@ -11,6 +11,41 @@
     // set up twig
 
     require_once 'php/twig.php';
+    function emailUsers($users) {
+      foreach($users as $user) {
+        $query = "SELECT email, first_name FROM user WHERE onid = ?";
+
+        $statement = $database -> prepare($query);
+
+        $statement -> bind_param('s', $user);
+        $statement -> execute();
+
+        $result = $statement -> get_result();
+        $result_array = $result -> fetch_all(MYSQLI_ASSOC);
+        $user_name = $result_array[0]['first_name'];
+        $user_email = $result_array[0]['email'];
+
+        // format email message
+        $format = "Hi %s,\n\nThe host for %s has removed the slot that you reserved.\n\n
+                  Please sign up for an available slot by clicking the link below\n\n
+                  %s";
+        $headers = "From: MyEventBoard" . "\r\n";
+        //$msg = sprintf($format, )
+      }
+
+    }
+
+    $added_slots = json_decode($_POST['added_slots'], true);
+    $deleted_slots = json_decode($_POST['deleted_slots'], true);
+
+
+
+
+
+
+
+
+
 
 
 
