0d826e11387a50869d55f6070fed2e37373b8a08
Blaise Takushi <takushib@flip2.engr.oregonstate.edu>
2020-05-12 16:48:52 -0700

    finished file upload button

--- reservations.php
+++ reservations.php
@@ -15,19 +15,19 @@
     // get data for time slots reserved by user from database
 
     $query = "
-        
+
         SELECT
-            t2.name AS 'Event Name', 
+            t2.name AS 'Event Name',
             t1.start_time AS 'Start Time',
             t2.location AS 'Location',
-			CONCAT(t4.first_name, ' ', t4.last_name) AS 'Creator Name',
+			CONCAT(t4.first_name, ' ', t4.last_name) AS 'Event Host',
             t2.hash AS 'Event Key',
             t1.hash AS 'Time Slot Key'
-        FROM 
+        FROM
             booking AS t0
-        INNER JOIN timeslot AS t1 
-            ON t0.fk_timeslot_id = t1.id 
-        INNER JOIN event AS t2 
+        INNER JOIN timeslot AS t1
+            ON t0.fk_timeslot_id = t1.id
+        INNER JOIN event AS t2
             ON t1.fk_event_id = t2.id
         INNER JOIN user AS t3
             ON t0.fk_user_id = t3.id
@@ -53,12 +53,12 @@
     // render page using twig
 
     echo $twig -> render(
-        'views/reservations.twig', 
+        'views/reservations.twig',
         [
             'user_ONID' => $_SESSION['user'],
-            'table_headers' => $resultKeys, 
+            'table_headers' => $resultKeys,
             'table_rows' => $resultArray
         ]
-    ); 
+    );
 
 ?>
