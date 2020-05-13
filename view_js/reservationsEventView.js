f42760bca21fb9b6ec223c586ced548708cbb2e5
liaoto <liaoto@oregonstate.edu>
2020-05-13 01:40:34 -0700

    file in reservations view

--- view_js/reservationsEventView.js
+++ view_js/reservationsEventView.js
@@ -58,12 +58,17 @@ function init() {
 	var locationOfEvent  = "Replace me with Location";
 	var eventCreator = "Replace me with Creator";
 	var description = "Replace me with Description";
+	var uploadedFile = "ReplaceWithFileName";
 	
 	var startTimeElement = $('<text>'+ startTime +'</text>');
 	var endTimeElement = $('<text>'+ endTime +'</text>');
 	var locationOfEventElement = $('<text>'+ locationOfEvent +'</text>');
 	var eventCreatorElement = $('<text>'+ eventCreator +'</text>');
 	var descriptionElement = $('<text>'+ description +'</text>');
+	var fileElement = $('<a>'+ uploadedFile +'</a>');
+	var downloadLinkToFile = "replacethiswithfile.txt";
+	fileElement.attr("href", downloadLinkToFile);
+	fileElement.attr("download", true);
 	
 	$('#eventStartTimeLabel').append(startTimeElement);
 	$('#eventEndTimeLabel').append(endTimeElement);
@@ -74,6 +79,11 @@ function init() {
 		$('#eventDescriptionLabel').append(descriptionElement);
 	else
 		$('#eventDescriptionLabel').remove();
+	
+	if (fileUploadCheck == true)
+		$('#eventFileLabel').append(fileElement);
+	else
+		$('#eventFileLabel').remove();
 }
 
 
